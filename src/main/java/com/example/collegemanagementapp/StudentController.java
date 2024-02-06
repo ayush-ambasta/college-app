b40ff46471dbbcdd92e8f09a8be362cc8d03c304
Ayush Ambasta <ayushambasta2001@gmail.com>
2024-02-06 14:42:31 +0530

    First Rest Api

---
+++ src/main/java/com/example/collegemanagementapp/StudentController.java
@@ -0,0 +1,37 @@
+package com.example.collegemanagementapp;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class StudentController {
+    Map<Integer,Student>studentDb=new HashMap<>();
+
+    @PostMapping("/student/add")
+    public String addStudent(@RequestBody Student student){
+        if(studentDb.containsKey(student.getId())){
+            return "Student Already Registered";
+        }
+        studentDb.put(student.getId(),student);
+        return "Student Registered Successfully";
+    }
+
+    @GetMapping("/student/get")
+    public Student getStudent(@RequestParam("id") int id){
+        return studentDb.get(id);
+    }
+
+    @GetMapping("/student/get/{name}/{q}")
+    public Student getStudentByName(@PathVariable("name") String name,@PathVariable("q") int age){
+        for(Integer x: studentDb.keySet()){
+            if(studentDb.get(x).getName().equals(name)){
+                studentDb.get(x).setAge(age);
+                return studentDb.get(x);
+            }
+        }
+        return null;
+    }
+
+}
